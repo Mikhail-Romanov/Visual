9f79b0eac9b42baf054b388c398ad1a1f8dea21d
Mikhail-Romanov <54847303+Mikhail-Romanov@users.noreply.github.com>
2019-11-14 20:25:41 +0300

    Add files via upload

---
+++ Consol.vb
@@ -0,0 +1,42 @@
+﻿
+
+
+Module Module1
+
+    Sub Main()
+        Console.BackgroundColor = ConsoleColor.Magenta 'любой цвет текст
+        Console.Clear() 'цвет фона
+        Console.WriteLine("Hello boy") 'текст ,что бужет внутри консолm
+
+
+        Console.ForegroundColor = ConsoleColor.Red
+        Console.BackgroundColor = ConsoleColor.Yellow
+        Console.WriteLine("Все будет хорошо") 'цвет текс фона
+        Console.ReadLine() 'Для того что бы команда была окончена
+
+
+
+
+        '      Console.Clear() 'при нажание enter будет переключена на строчку ,что с низу
+        '     Console.WriteLine()
+        '    Console.WriteLine() 'если ничего не написать, то просто удет пустая строчка 
+        '   Console.WriteLine()
+        '  Console.WriteLine()
+        ' Console.WriteLine()
+        'Console.WriteLine()
+        'Console.WriteLine()
+        'Console.WriteLine()
+        'Console.WriteLine()
+        'Console.WriteLine()
+        'Console.WriteLine("Сколько тебе лет?")
+        'Console.ReadLine()
+        'Console.Clear()
+        'Console.SetCursorPosition(10, 40) ' задает позицию
+        'Console.WriteLine("                                     Ты просто молодец!") 'если вы будите двигать текст в права, то и в консоле оно тоже будет двигаться 
+        'Console.ReadLine()
+
+
+
+    End Sub
+
+End Module
